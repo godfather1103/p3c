f2015faebec8906dfb618123ed96a6e9b3000543
Jack Chu <chuchuanbao@gmail.com>
2023-08-08 11:05:26 +0800

    docs(描述): 优化描述

--- idea-plugin/p3c-idea/build.gradle.kts
+++ idea-plugin/p3c-idea/build.gradle.kts
@@ -39,11 +39,13 @@ tasks {
         pluginId.set("io.github.godfather1103.alibaba.p3c")
         pluginDescription.set(
             """
-<h2>English Readme：</h2>
+<h1>English Readme：</h1>
+<h2>The plugin conflicts with the official plugin. Please uninstall the original plugin before installing this plugin</h2>
 <p>Alibaba Java Coding Guidelines plugin support.Fix some bug.such as <a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>,<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a></p>
 <p>Official plugin URL: <a href="https://plugins.jetbrains.com/plugin/10046-alibaba-java-coding-guidelines">Alibaba Java Coding Guidelines</a></p>
 
-<h2>中文说明：</h2>
+<h1>中文说明：</h1>
+<h2>插件与官方插件会冲突，请先卸载原插件，再安装本插件</h2>
 <p>阿里巴巴Java编码规范检查插件。修复了一些官方一直未修复的Bug。如 <a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>,<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a></p>
 <p>官方插件地址：<a href="https://plugins.jetbrains.com/plugin/10046-alibaba-java-coding-guidelines">Alibaba Java Coding Guidelines</a></p>
 
@@ -52,14 +54,14 @@ tasks {
         changeNotes.set(
             """
         <ul>
-        1.0
-        <li>fix(<a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>): 1、修复2022.1中检测到缺失override注解出现异常；2、修复编译异常</li>
-        <li>fix(<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a>): 修复驼峰等自动修改异常</li>
-        </ul>
-        <ul>
         1.1
         <li>优化对于2023.2的API调度</li>
         <li>优化对应分析结果信息分组</li>
+        </ul>
+        <ul>
+        1.0
+        <li>fix(<a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>): 1、修复2022.1中检测到缺失override注解出现异常；2、修复编译异常</li>
+        <li>fix(<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a>): 修复驼峰等自动修改异常</li>
         </ul>
         """.trimIndent()
         )
