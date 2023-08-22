69c6406a02d28728bbf96d0c810780a4394a189b
Jack Chu <chuchuanbao@gmail.com>
2023-08-22 19:14:00 +0800

    feat(p3c-pmd): 通过反射方式注入相关变动的信息

--- idea-plugin/p3c-common/src/main/kotlin/io/github/godfather1103/rule/impl/PojoMustOverrideToStringRule.kt
+++ idea-plugin/p3c-common/src/main/kotlin/io/github/godfather1103/rule/impl/PojoMustOverrideToStringRule.kt
@@ -4,13 +4,28 @@ import com.alibaba.p3c.pmd.lang.java.util.PojoUtils
 import io.github.godfather1103.rule.IModifyRuleValue
 import io.github.godfather1103.service.BaseNameListServiceExt
 
+/**
+ * <p>Title:        Godfather1103's Github</p>
+ * <p>Copyright:    Copyright (c) 2023</p>
+ * <p>Company:      https://github.com/godfather1103</p>
+ *
+ * @author  作者: Jack Chu E-mail: chuchuanbao@gmail.com
+ * @date 创建时间：2023/8/22 19:00
+ * @version 1.0
+ * @since  1.0
+ */
 class PojoMustOverrideToStringRule : IModifyRuleValue {
     override fun modifyValue(base: BaseNameListServiceExt, key: String) {
-        PojoUtils::class.java.declaredFields.find { it.name == "POJO_SUFFIX_SET" }?.apply {
-            isAccessible = true
-            val list = get(null) as MutableList<String>
-            list?.clear()
-            base.getNameList(className(), key)?.forEach { list.add(it) }
+        when (key) {
+            "POJO_SUFFIX_SET" -> PojoUtils::class.java
+                .declaredFields.find { it.name == key }?.apply {
+                    isAccessible = true
+                    val list = get(null) as MutableList<String>
+                    list.clear()
+                    base.getNameList(className(), key)
+                        .forEach { list.add(it) }
+                }
         }
+
     }
 }
