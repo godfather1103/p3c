c0df1b3d3f6710c660fcf3f7238fadaa6cab486b
Jack Chu <chuchuanbao@gmail.com>
2023-08-22 23:00:32 +0800

    feat(p3c-pmd): 补充相关实现类

---
+++ idea-plugin/p3c-common/src/main/kotlin/io/github/godfather1103/rule/impl/ClassNamingShouldBeCamelRule.kt
@@ -0,0 +1,20 @@
+package io.github.godfather1103.rule.impl
+
+import io.github.godfather1103.rule.IModifyRuleValue
+import io.github.godfather1103.service.BaseNameListServiceExt
+
+/**
+ * <p>Title:        Godfather1103's Github</p>
+ * <p>Copyright:    Copyright (c) 2023</p>
+ * <p>Company:      https://github.com/godfather1103</p>
+ *
+ * @author  作者: Jack Chu E-mail: chuchuanbao@gmail.com<BR>
+ * 创建时间：2023-08-22 22:58
+ * @version 1.0
+ * @since  1.0
+ */
+class ClassNamingShouldBeCamelRule : IModifyRuleValue {
+    override fun modifyValue(base: BaseNameListServiceExt, key: String) {
+
+    }
+}
