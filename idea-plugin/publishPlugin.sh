99fd5daa170a0f0fc093053ada8d83aee8851b75
Jack Chu <chuchuanbao@gmail.com>
2024-01-19 15:59:28 +0800

    refactor(code): 优化构建脚本

--- idea-plugin/publishPlugin.sh
+++ idea-plugin/publishPlugin.sh
@@ -3,8 +3,8 @@ idea_versions=("2022.1" "2022.2" "2022.3" "2023.1" "2023.2")
 for version in "${idea_versions[@]}"
 do
   echo "开始构建$version"
-  ./gradlew -Didea_version=$version clean :p3c-idea:buildPlugin
+  ./gradlew -Didea_version=$version clean :p3c-idea:publishPlugin
 done
 
 echo "构建最新版本"
-./gradlew -Didea_version=$version clean :p3c-idea:buildPlugin
+./gradlew -Didea_version=$version clean :p3c-idea:publishPlugin
