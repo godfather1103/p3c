3878027337111c3f9f1774b6dc0f5121b7ff9dd0
Jack Chu <chuchuanbao@gmail.com>
2025-07-01 14:08:42 +0800

    build(idea-plugin): 更新插件描述并移除构建配置中的重复信息
    - 从 build.gradle.kts 中移除 pluginDescription 设置- 在 plugin.xml 中添加详细的插件描述，包括英文和中文说明
    - 保留 changeNotes 在 build.gradle.kts 中的设置
    
    Signed-off-by: Jack Chu <chuchuanbao@gmail.com>

--- idea-plugin/p3c-idea/build.gradle.kts
+++ idea-plugin/p3c-idea/build.gradle.kts
@@ -21,20 +21,6 @@ tasks {
         sinceBuild.set("${yearVersion}${noVersion}.0")
 //        untilBuild.set("${yearVersion}${noVersion}.*")
         pluginId.set("io.github.godfather1103.alibaba.p3c")
-        pluginDescription.set(
-            """
-<h1>English Readme：</h1>
-<h2>The plugin conflicts with the official plugin. Please uninstall the original plugin before installing this plugin</h2>
-<p>Alibaba Java Coding Guidelines plugin support.Fix some bug.such as <a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>,<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a></p>
-<p>Official plugin URL: <a href="https://plugins.jetbrains.com/plugin/10046-alibaba-java-coding-guidelines">Alibaba Java Coding Guidelines</a></p>
-
-<h1>中文说明：</h1>
-<h2>插件与官方插件会冲突，请先卸载原插件，再安装本插件</h2>
-<p>阿里巴巴Java编码规范检查插件。修复了一些官方一直未修复的Bug。如 <a href="https://github.com/alibaba/p3c/issues/898">issues-898</a>,<a href="https://github.com/alibaba/p3c/issues/900">issues-900</a></p>
-<p>官方插件地址：<a href="https://plugins.jetbrains.com/plugin/10046-alibaba-java-coding-guidelines">Alibaba Java Coding Guidelines</a></p>
-
-        """.trimIndent()
-        )
         changeNotes.set(
             """
         <ul>
